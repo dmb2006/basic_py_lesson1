b81bd7b4f2d35156b7efcad3b622790822372cbd
Aleksandr Minin <dmb2006minin@yandex.ru>
2025-05-28 19:10:59 +0300

    added test dicts and enique elements

--- test_hw.py
+++ test_hw.py
@@ -74,30 +74,33 @@ def test_random_list():
     assert all(l[i] <= l[i + 1] for i in range(len(l) - 1))
 
 
-# def test_unique_elements():
-#     """
-#     Удалите из списка все повторяющиеся элементы
-#     """
-#     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
-#     # TODO удалите повторяющиеся элементы
-#
-#     assert isinstance(l, list)
-#     assert len(l) == 10
-#     assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-#
-#
-# def test_dicts():
-#     """
-#     Создайте словарь из двух списков.
-#     Используйте первый список как ключи, а второй - как значения.
-#     Подсказка: используйте встроенную функцию zip.
-#     """
-#     first = ["a", "b", "c", "d", "e"]
-#     second = [1, 2, 3, 4, 5]
-#     # TODO создайте словарь
-#     d = {}
-#
-#     assert isinstance(d, dict)
-#     assert len(d) == 5
-#     assert list(d.keys()) == first
-#     assert list(d.values()) == second
+def test_unique_elements():
+    """
+    Удалите из списка все повторяющиеся элементы
+    """
+    l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
+    l = list(set(l))
+    # TODO удалите повторяющиеся элементы
+
+    assert isinstance(l, list)
+    assert len(l) == 10
+    assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def test_dicts():
+    """
+    Создайте словарь из двух списков.
+    Используйте первый список как ключи, а второй - как значения.
+    Подсказка: используйте встроенную функцию zip.
+    """
+    first = ["a", "b", "c", "d", "e"]
+    second = [1, 2, 3, 4, 5]
+    # TODO создайте словарь
+    d = dict(zip(first, second))
+    print(d.keys())
+    print(d.values())
+
+    assert isinstance(d, dict)
+    assert len(d) == 5
+    assert list(d.keys()) == first
+    assert list(d.values()) == second
